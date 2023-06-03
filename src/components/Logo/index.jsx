5ba58543d2ea63c2ecc7376d1acbf4eb0faacfbe
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-06-03 12:18:31 -0300

    Implementando redirect no logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,20 +1,23 @@
 import { Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 import { memo } from "react";
 
-const Logo = ({ label = false }) => {
+const Logo = () => {
   return (
-    <Flex gap={2}>
-      <Image src="/logo.png" width={36} height={36} />
-      <Flex>
-        <Text color="#1A4B86" fontWeight="semibold" fontSize={25}>
-          RACHI
-        </Text>
-        <Text color="#5FA0FF" fontWeight="semibold" fontSize={25}>
-          NHA
-        </Text>
+    <Link href={"/"} passHref>
+      <Flex gap={2}>
+        <Image src="/logo.png" width={36} height={36} />
+        <Flex>
+          <Text color="#1A4B86" fontWeight="semibold" fontSize={25}>
+            RACHI
+          </Text>
+          <Text color="#5FA0FF" fontWeight="semibold" fontSize={25}>
+            NHA
+          </Text>
+        </Flex>
       </Flex>
-    </Flex>
+    </Link>
   );
 };
 
