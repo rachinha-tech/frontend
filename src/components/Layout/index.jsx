465779e6593d5025380f9991492e7c92e2c8c37e
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-06-03 23:35:38 -0300

    Texto informativo

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -11,19 +11,29 @@ export default function Layout({ children }) {
 
   return (
     <Flex display={widthDisplay} h="full" gap={"10"} flexDirection="column">
-      <Flex as={"header"} position="fixed" zIndex={1} w="full">
-        <NavBar />
-      </Flex>
+      {widthDisplay.lg || widthDisplay.xl ? (
+        <Center>
+          <Text fontWeight={"bold"} color={"blue.800"}>
+            Aplicação funciona apenas em dispositivos móveis
+          </Text>
+        </Center>
+      ) : (
+        <>
+          <Flex as={"header"} position="fixed" zIndex={1} w="full">
+            <NavBar />
+          </Flex>
 
-      <Box as={"main"} padding={2} mt={16}>
-        {children}
-      </Box>
+          <Box as={"main"} padding={2} mt={16}>
+            {children}
+          </Box>
 
-      <Flex as={"footer"} justify={"center"} padding={2}>
-        <Text fontSize={"xs"} fontWeight={"semibold"} color={"blue.800"}>
-          &copy; Desenvolvido por Rachinha Tech | 2023
-        </Text>
-      </Flex>
+          <Flex as={"footer"} justify={"center"} padding={2}>
+            <Text fontSize={"xs"} fontWeight={"semibold"} color={"blue.800"}>
+              &copy; Desenvolvido por Rachinha Tech | 2023
+            </Text>
+          </Flex>
+        </>
+      )}
     </Flex>
   );
 }
