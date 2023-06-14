1dbc56f4d8a189a2acb81b3a3cfe7b3b4f721dcd
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-06-14 12:15:22 -0300

    Ajustes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withOptimizedImages = require("next-optimized-images");
 const withPWA = require("next-pwa");
 
 const nextConfig = withOptimizedImages({
-  optimizeImages: false, 
+  optimizeImages: true, 
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.optimization.splitChunks.cacheGroups = {
