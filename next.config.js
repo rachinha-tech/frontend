7845759040354a26971e595f3a8bfd7a9ce3242a
gabriell-silva <78451669+gabriell-silva@users.noreply.github.com>
2023-06-14 09:56:34 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const withPWA = require("next-pwa");
+const withImages = require('next-images');
+const withPWA = require('next-pwa');
 
-module.exports = withPWA({
-  dest: "public",
-  // disable: process.env.NODE_ENV === 'development',
-});
+module.exports = withImages(withPWA({
+  dest: 'public',
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+  },
+}));
