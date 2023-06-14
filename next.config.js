ef40cfb3f92f7ccbff0d20d04650c97ba3a92656
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-06-14 11:53:23 -0300

    retirando key pwa

--- next.config.js
+++ next.config.js
@@ -4,8 +4,5 @@ const withPWA = require("next-pwa");
 module.exports = withPWA(
   withOptimizedImages({
     dest: "public",
-    pwa: {
-      disable: process.env.NODE_ENV === "development",
-    },
   })
 );
